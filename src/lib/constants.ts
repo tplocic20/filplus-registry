fbb58c3ca0d941566de7b0910e0f5d90d0d8eead
Charly López <clriesco@gmail.com>
2023-10-26 14:51:57 +0200

    Modify colors

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,19 +1,19 @@
 export const requestTypeColor = {
   New: 'bg-blue-500 text-white',
-  Refill: 'bg-green-500 text-white',
+  Refill: 'bg-teal-500 text-white',
   default: 'bg-gray-400 text-white',
 }
 
 export const stateColor = {
   GovernanceReview: 'bg-gray-400 text-white',
-  Proposal: 'bg-blue-500 text-white',
-  Approval: 'bg-green-500 text-white',
-  Confirmed: 'bg-purple-500 text-white',
+  Proposal: 'bg-sky-400 text-white',
+  Approval: 'bg-rose-400 text-white',
+  Confirmed: 'bg-purple-400 text-white',
 }
 
 export const allocationActiveColor = {
   inactive: 'bg-gray-400 text-white',
-  active: 'bg-blue-400 text-white',
+  active: 'bg-green-500 text-white',
 }
 
 export const stateMapping = {
