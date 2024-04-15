b904d59f434a8a8f4596d919cb7a3d0f8c5cef51
Alexandru-Marian Constantin <alexm.constantin@gmail.com>
2024-04-15 12:34:33 +0300

    fix: set to manual directly the input for amount

--- src/components/cards/AppInfoCard.tsx
+++ src/components/cards/AppInfoCard.tsx
@@ -355,6 +355,7 @@ const AppInfoCard: React.FC<ComponentProps> = ({
       application.Lifecycle.State === 'ReadyToSign' &&
       application['Allocation Requests'].length > 1
     ) {
+      setAllocationType('manual');
       setAllocationAmount(
         application['Allocation Requests'].find((e) => e.Active)?.[
           'Allocation Amount'
